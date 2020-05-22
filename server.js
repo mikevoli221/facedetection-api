a19b801b198d11e48c43adf7d15c9c70837c2940
Wirecard Vietnam <hiep.ho@wirecard.com>
2020-05-22 15:54:04 +0700

    signin and register api

--- server.js
+++ server.js
@@ -1,9 +1,99 @@
 const express = require('express');
 
+const database = {
+    users : [
+        {
+            id : 123,
+            name : 'hiep',
+            email : 'hiep@yahoo.com',
+            password : '12345',
+            entries : 0,
+            joined : new Date()
+        },
+        {
+            id : 124,
+            name : 'kim',
+            email : 'kim@yahoo.com',
+            password : '12345',
+            entries : 0,
+            joined : new Date()
+        },
+        {
+            id : 125,
+            name : 'binh',
+            email : 'binh@yahoo.com',
+            password : '12345',
+            entries : 0,
+            joined : new Date()
+        },
+        {
+            id : 126,
+            name : 'an',
+            email : 'an@yahoo.com',
+            password : '12345',
+            entries : 0,
+            joined : new Date()
+        }
+    ]
+};
+
+
 const app = express();
 app.use(express.json());
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('FaceDectection API is listening on port 3000')
+});
 
 app.get('/', (req, res) => {
-    res.send('Setting Express Okay');
+    res.json('Setting Express Okay');
 })
+
+app.post('/signin', (req, res) => {
+    const {email, password} = req.body;
+    const isValidUser = database.users.some(user => {
+        if (user.email === email && user.password === password){
+            return true;
+        }
+    })
+    
+    isValidUser ? res.json('Signed in succesfully') : res.status(400).json('Signed in failed') 
+})
+
+app.post('/register', (req, res) => {
+    const {name, email, password} = req.body;
+    const newUser = {
+        id : 111,
+        name : name,
+        email : email,
+        password: password,
+        entries : 0,
+        joined : new Date()
+    }
+
+    const isDuplicateUser = database.users.some(user => {
+        if (user.email === email){
+            return true;
+        }
+    })
+
+    if (isDuplicateUser){
+        res.status(400).json('User is already existed');
+    }else{
+        database.users.push(newUser);
+        res.json('Registered user succesfully');
+    }
+
+
+    console.log(database);
+
+})
+
+/*
+
+/ res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/

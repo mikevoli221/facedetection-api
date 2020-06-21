93f412893580b591d00189134c517590ceba1514
Hiep Ho <mikevoli221@yahoo.com>
2020-06-21 17:43:07 +0700

    update api key variable

--- controllers/image.js
+++ controllers/image.js
@@ -37,7 +37,7 @@ const updateEntries = (req, res, database) => {
 
 const callClarifaiAPI = (req, res) => {
     const app = new Clarifai.App({
-        apiKey: '45a608a8646e432ebaf8e53764b6b51a'
+        apiKey: process.env.CLARIFAI_API_KEY
     });
 
     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.imageUrl)
